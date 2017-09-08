0208df5860e3524daec1a37fd37855ce28dc5d7a
Marcelo Soares <marcelo.rs@gmail.com>
2017-09-08 11:17:44 -0300

    first commit

---
+++ upload/admin/language/portuguese-br/shipping/fixoporcep.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']    = 'Frete fixo por CEP';
+
+// Text
+$_['text_shipping']    = 'Frete';
+$_['text_success']     = 'Frete fixo por CEP modificado com sucesso!';
+$_['text_edit']        = 'Configurações do Frete fixo';
+$_['text_first_zip']   = 'Faixa Inicial de CEP';
+$_['text_last_zip']    = 'Faixa Final de CEP';
+
+// Entry
+$_['entry_total']      = 'Total mínimo';
+$_['entry_cost']       = 'Valor fixo';
+$_['entry_tax_class']  = 'Grupo de impostos';
+$_['entry_geo_zone']   = 'Região geográfica';
+$_['entry_status']     = 'Situação';
+$_['entry_sort_order'] = 'Posição';
+$_['entry_first_zip']  = 'Faixa Inicial de CEP';
+$_['entry_last_zip']  = 'Faixa Final de CEP';
+
+// Help
+$_['help_total']       = 'O valor mínimo que o total do pedido deve alcançar para que o Frete fixo seja oferecido como opção de frete ao cliente.';
+
+// Error
+$_['error_permission'] = 'Atenção: Você não tem permissão para modificar o Frete Fixo por CEP!';
